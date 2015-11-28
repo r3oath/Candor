cc0ed6f694a8bcc2ec952b4e5e998e026ff564ea
Tristan Strathearn <r3oath@gmail.com>
2015-11-28 21:25:52 +1000

    Base splash page implementation.

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,18 @@ var gulp   = require('gulp');
 var candor = require('gulp-candor');
 var stylus = require('gulp-stylus');
 
-gulp.task('default', function() {
+gulp.task('default', ['candor', 'stylus']);
+
+gulp.task('candor', function() {
     return gulp
         .src('index.cdor')
         .pipe(candor())
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('stylus', function() {
+    return gulp
+        .src('app.styl')
+        .pipe(stylus())
         .pipe(gulp.dest('./'));
 });
